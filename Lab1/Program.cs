c5bb351aca1c7f24a7eeb9e7e610ad0da040e54d
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-10-29 13:51:21 -0400

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml; // using xml reader
+using System.Xml.Serialization; // used to create the XML Serialization
+using System.IO;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.ID = 123;
+            emp.Name = "Ninja Nerd";
+            emp.SSNumber = 123456789;
+            emp.EntryDate = DateTime.Now;
+
+            Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Name: {emp.Name}\nEmployee SS#: {emp.SSNumber} \nDate Created: {emp.EntryDate}");
+            Console.ReadLine();
+
+            string FilePath = "C:\\Users\\neely_tristian\\Documents\\";
+            string FileName = "Ninja.xml";
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+
+            XmlSerializer ser = new XmlSerializer(typeof(Employee));
+
+            ser.Serialize(writer, emp);
+            writer.Close();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
